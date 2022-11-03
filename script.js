770d5855ce41e50455697c0ba7ced124e2b9ea31
darkbits018 <abhaygp18@gmail.com>
2022-11-03 22:12:02 +0530

    initial style

---
+++ script.js
@@ -0,0 +1,56 @@
+//close
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i=0; i<myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+//hide on close click
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+//checked symbol
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'Li') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+//new list
+function newElement () {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("write something UwU");
+    } else {
+        document.getElementById("unlist").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i=0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+}
